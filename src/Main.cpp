15e5f30c77474edf17330caa15a27784d483cf81
shidalj <shidalj@1J000WL-1LB8HW3>
2024-11-06 09:16:06 -0600

    added exam code

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,10 +6,23 @@
 */
 
 #include <iostream>
+#include "my_string.h"
 
 using namespace std;
 
 int main() {
-    cout << "hello world" << endl;
+    const char* h = "hello";
+    my_string s(h, 5); // 4
+    s.print(); // 5
+    my_string s2(s); // 6
+    s2[0] = 'm';
+    s.print(); // 8
+    s = s2; // 9
+    s.print(); // 10
+    s2[0] = 'y';
+    // cout << s[5] << endl; // 12
+    s = s + s2; // 13
+    s.print(); // 14
+
     return 0;
 }
